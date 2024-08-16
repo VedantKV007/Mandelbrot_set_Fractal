8e3577f1a23bd1618327f30159f2f014daabc9da
Vedantgosling <153468680+Vedantgosling@users.noreply.github.com>
2024-08-16 10:19:50 +0200

    Add project files.

---
+++ complex.hpp
@@ -0,0 +1,26 @@
+struct Complex
+{
+	double real;
+	double imaginary;
+
+	Complex(const double& re, const double& im) : real(re), imaginary(im) {}
+
+	Complex operator += (const Complex& other)
+	{
+		real += other.real;
+		imaginary += other.imaginary;
+		return *this;
+	}
+
+	void sqr()
+	{
+		double temp = (real * real) - (imaginary * imaginary);
+		imaginary = (2 * real * imaginary);
+		real = temp;
+	}
+
+	bool pointNotFits() const
+	{
+		return (real * real) + (imaginary * imaginary) > 4;
+	}
+};
